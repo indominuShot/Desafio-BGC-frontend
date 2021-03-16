5600488d22154082391ac6c0a6da03a18f99edc3
indominuShot <luang193@gmail.com>
2021-03-15 22:47:57 -0300

    Rota da página de comprar adicionada

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './Pages/Login';
+import Shop from './Pages/Shop';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Login} />
+        <Route path="/shop" exact component={Shop} />
       </Switch>
     </BrowserRouter>
   );
