12d7e0893b852b084bc19b9d0909e2a97a666dbb
indominuShot <luang193@gmail.com>
2021-03-15 22:48:47 -0300

    Form estilizado

--- src/Components/FormLogin/index.tsx
+++ src/Components/FormLogin/index.tsx
@@ -1,12 +1,47 @@
-import { Container, Form, FormHeader, FormMain, FormFooter } from './styles';
+import { useHistory } from 'react-router-dom';
+
+import {
+  Container,
+  Form,
+  FormHeader,
+  FormMain,
+  FormFooter,
+  Input,
+} from './styles';
 
 export default function FormLogin() {
+  const route = useHistory();
+
+  function handleLogin() {
+    route.push('/shop');
+  }
+
   return (
     <Container>
       <Form>
-        <FormHeader></FormHeader>
-        <FormMain></FormMain>
-        <FormFooter></FormFooter>
+        <FormHeader>
+          <h3>Faça o Login</h3>
+        </FormHeader>
+
+        <FormMain>
+          <div>
+            <span>Usuário</span>
+            <Input type="text" placeholder="Digite..." />
+          </div>
+
+          <div>
+            <span>Senha</span>
+            <Input type="text" placeholder="Digite..." />
+          </div>
+        </FormMain>
+
+        <FormFooter>
+          <button type="button" onClick={handleLogin}>
+            Login
+          </button>
+          <span>ou</span>
+          <button type="button">Cadastrar</button>
+        </FormFooter>
       </Form>
     </Container>
   );
--- src/Components/FormLogin/styles.ts
+++ src/Components/FormLogin/styles.ts
@@ -8,11 +8,16 @@ export const Container = styled.div`
   align-items: center;
   justify-content: center;
 
-  padding: 4em 2.8rem;
+  padding: 4rem 2.8rem;
+
+  @media (max-width: 500px) {
+    padding: 2rem 1rem;
+  }
 `;
 
 export const Form = styled.form`
   margin: 0 auto;
+  padding: 2rem 4rem;
   height: 100%;
   width: 100%;
   max-width: 28.875rem;
@@ -24,10 +29,69 @@ export const Form = styled.form`
 
   border-radius: 8px;
   box-shadow: 0 0 32px #000;
+
+  @media (max-width: 425px) {
+    padding: 2rem;
+  }
 `;
 
-export const FormHeader = styled.header``;
+export const FormHeader = styled.header`
+  width: 100%;
 
-export const FormMain = styled.main``;
+  > h3 {
+    font-size: 1.8rem;
+    text-align: center;
+  }
+`;
 
-export const FormFooter = styled.footer``;
+export const FormMain = styled.main`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+
+    > span {
+      font-size: 1rem;
+      font-weight: 600;
+      color: #000;
+    }
+  }
+
+  > div + div {
+    margin-top: 2rem;
+  }
+`;
+
+export const Input = styled.input`
+  width: 100%;
+
+  & + input {
+    margin-top: 1.5rem;
+  }
+`;
+
+export const FormFooter = styled.footer`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 2rem;
+  width: 100%;
+
+  > span {
+    font-size: 1rem;
+    font-weight: 600;
+    margin: 0.4rem;
+  }
+
+  > button {
+    width: 100%;
+  }
+`;
