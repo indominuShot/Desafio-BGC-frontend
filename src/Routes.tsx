23e6fd1476cd18c5327b51705eeeb7ca86726188
indominuShot <luang193@gmail.com>
2021-03-19 19:28:39 -0300

    Header inserido; Rota de login, e registro adicionadas; Rota do compras alterada

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,13 +1,18 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from './Components/Header';
 import Login from './Pages/Login';
+import Register from './Pages/Register';
 import Shop from './Pages/Shop';
 
 export default function Routes() {
   return (
     <BrowserRouter>
+      <Header />
+
       <Switch>
-        <Route path="/" exact component={Login} />
-        <Route path="/shop" exact component={Shop} />
+        <Route path="/" exact component={Shop} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/register" exact component={Register} />
       </Switch>
     </BrowserRouter>
   );
