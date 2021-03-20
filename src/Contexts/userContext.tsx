ff67e89d0d3610b5b8e39495fef5321c54acc308
indominuShot <luang193@gmail.com>
2021-03-20 16:58:17 -0300

    Controlando verificação; alterando credenciais do usuário; função de login

--- src/Contexts/userContext.tsx
+++ src/Contexts/userContext.tsx
@@ -5,14 +5,18 @@ import { toast } from 'react-toastify';
 import _AwsLoginResponse from '../@types/_AwsLoginResponse';
 import _AuthAwsError from '../@types/_AuthAwsError';
 import verifyUserAccess from '../Utils/verifyUserAccess';
+import { useHistory } from 'react-router';
+import _UserCredentials from '../@types/_UserCredentials';
 
 interface _UserContextProps {
   isLogged: boolean;
-  handleUserLogin: (
-    email: string,
-    password: string
-  ) => Promise<boolean>;
+  isWaitingVerificationCode: boolean;
+  userCredentials: _UserCredentials;
+  handleUserCredentials: (email: string, password: string) => void;
+  waitForVerificationCode: () => void;
+  cancelWaitForVerificationCode: () => void;
   handleUserLogout: () => Promise<void>;
+  handleUserLogin: (email: string, password: string) => Promise<boolean>;
 }
 
 interface _UserProviderProps {
@@ -21,12 +25,18 @@ interface _UserProviderProps {
 
 export const userContext = createContext({} as _UserContextProps);
 
-export default function UserProvider({
-  children,
-}: _UserProviderProps) {
+export default function UserProvider({ children }: _UserProviderProps) {
+  const [userCredentials, setUserCredentials] = useState(
+    {} as _UserCredentials
+  );
   const [isLogged, setIslogged] = useState(
     localStorage.getItem('userData') ? true : false
   );
+  const [isWaitingVerificationCode, setIsWaitingVerificationCode] = useState(
+    false
+  );
+
+  const route = useHistory();
 
   async function handleUserLogin(email: string, password: string) {
     const loginSucceeded = Auth.signIn({
@@ -40,7 +50,7 @@ export default function UserProvider({
         });
 
         setIslogged(true);
-        toast.success('Logado com sucesso!', {
+        toast.success('Seja bem vindo!', {
           pauseOnHover: false,
           autoClose: 3000,
         });
@@ -49,8 +59,16 @@ export default function UserProvider({
         return true;
       })
       .catch((error: _AuthAwsError) => {
-        verifyUserAccess(error.message);
-        setIslogged(false);
+        if (error.message.indexOf('User is not confirmed') >= 0) {
+          handleUserCredentials(email, password);
+
+          Auth.resendSignUp(email);
+          route.push('/register');
+          setIsWaitingVerificationCode(true);
+        } else {
+          verifyUserAccess(error.message);
+          setIslogged(false);
+        }
         return false;
       });
 
@@ -68,12 +86,32 @@ export default function UserProvider({
       });
   }
 
+  function waitForVerificationCode() {
+    setIsWaitingVerificationCode(true);
+  }
+
+  function cancelWaitForVerificationCode() {
+    setIsWaitingVerificationCode(false);
+  }
+
+  function handleUserCredentials(email: string, password: string) {
+    setUserCredentials({
+      email,
+      password,
+    });
+  }
+
   return (
     <userContext.Provider
       value={{
         isLogged,
+        isWaitingVerificationCode,
+        userCredentials,
+        handleUserCredentials,
         handleUserLogin,
         handleUserLogout,
+        waitForVerificationCode,
+        cancelWaitForVerificationCode,
       }}
     >
       {children}
