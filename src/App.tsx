71d00b7d284463859e4ea001ff30b1f165f909da
indominuShot <luang193@gmail.com>
2021-03-20 17:01:33 -0300

    Contextos removidos

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,19 @@ import Amplify from 'aws-amplify';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-import { ShoppingCartProvider } from './Contexts/ShoppingCarContext';
 import Routes from './Routes';
-import awsConfig from './aws-exports';
+import awsConfig from './Services/awsConfig';
 
 import './Styles/global.css';
-import UserProvider from './Contexts/userContext';
 
 Amplify.configure(awsConfig);
 
 function App() {
   return (
     <div className="app">
-      <ShoppingCartProvider>
-        <UserProvider>
-          <Routes />
-        </UserProvider>
-        <ToastContainer />
-      </ShoppingCartProvider>
+      <Routes />
+
+      <ToastContainer />
     </div>
   );
 }
