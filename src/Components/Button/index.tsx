bf55c0de612b479864560bd2135914e6d57a9634
indominuShot <luang193@gmail.com>
2021-03-20 17:01:16 -0300

    LockIcon removido

--- src/Components/Button/index.tsx
+++ src/Components/Button/index.tsx
@@ -1,5 +1,4 @@
 import { AiOutlineLoading as LoadIcon } from 'react-icons/ai';
-import { FaLock as LockIcon } from 'react-icons/fa';
 
 import _Button from '../../@types/_Button';
 import { Container } from './styles';
@@ -13,16 +12,6 @@ export default function Button({
   return (
     <Container {...props} hoverColor={hoverColor} isLoading={isLoading}>
       {isLoading ? <LoadIcon /> : children}
-      {props.disabled && (
-        <div>
-          <LockIcon
-            style={{
-              animation: 'none',
-              marginLeft: '1rem',
-            }}
-          />
-        </div>
-      )}
     </Container>
   );
 }
