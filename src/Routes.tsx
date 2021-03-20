55eeee8b6206597cf27d5b3adaf71a5a34905bdd
indominuShot <luang193@gmail.com>
2021-03-20 16:56:29 -0300

    Contextos adicionados

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './Components/Header';
+import { ShoppingCartProvider } from './Contexts/ShoppingCarContext';
+import UserProvider from './Contexts/userContext';
 import Login from './Pages/Login';
 import Register from './Pages/Register';
 import Shop from './Pages/Shop';
@@ -7,13 +9,17 @@ import Shop from './Pages/Shop';
 export default function Routes() {
   return (
     <BrowserRouter>
-      <Header />
+      <ShoppingCartProvider>
+        <UserProvider>
+          <Header />
 
-      <Switch>
-        <Route path="/" exact component={Shop} />
-        <Route path="/login" exact component={Login} />
-        <Route path="/register" exact component={Register} />
-      </Switch>
+          <Switch>
+            <Route path="/" exact component={Shop} />
+            <Route path="/login" exact component={Login} />
+            <Route path="/register" exact component={Register} />
+          </Switch>
+        </UserProvider>
+      </ShoppingCartProvider>
     </BrowserRouter>
   );
 }
