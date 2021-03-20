b32bab516ec30088126242d357ac888f6eff6b95
indominuShot <luang193@gmail.com>
2021-03-20 16:57:09 -0300

    Exibindo produtos dinamicamente

--- src/Pages/Shop/index.tsx
+++ src/Pages/Shop/index.tsx
@@ -1,16 +1,25 @@
+import { API } from 'aws-amplify';
+import { useEffect, useState } from 'react';
+
 import ShopItem from '../../Components/ShopItem';
 import { Container, ItemList } from './styles';
 
 export default function Shop() {
+  const [minions, setMinions] = useState([]);
+
+  useEffect(() => {
+    API.get('serverless', '/', {
+      headers: {},
+      response: true,
+    }).then((e) => setMinions(e.data.Items));
+  }, []);
+
   return (
     <Container>
       <ItemList>
-        <ShopItem />
-        <ShopItem />
-        <ShopItem />
-        <ShopItem />
-        <ShopItem />
-        <ShopItem />
+        {minions.map((minion: any) => (
+          <ShopItem key={minion.id} data={minion} />
+        ))}
       </ItemList>
     </Container>
   );
