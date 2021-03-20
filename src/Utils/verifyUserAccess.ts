6be11319d4f8ee93e89ea90155cea266bbb4afd7
indominuShot <luang193@gmail.com>
2021-03-20 16:55:40 -0300

    Mensagem com 'usuário' modificadas para 'cliente'

--- src/Utils/verifyUserAccess.ts
+++ src/Utils/verifyUserAccess.ts
@@ -7,15 +7,17 @@ const toastConfig = {
 
 export default function verifyUserAccess(message: string) {
   if (message.indexOf('not exist') >= 0) {
-    toast.error('Usuário não existe', toastConfig);
+    toast.error('Cliente não existe', toastConfig);
+  } else if (message.indexOf('Incorrect username or password.') >= 0) {
+    toast.error('Cliente ou senha incorretos!', toastConfig);
   } else if (message.indexOf('email already exists') >= 0) {
-    toast.error('Usuário já existe!', toastConfig);
+    toast.error('Cliente já existe!', toastConfig);
   } else if (message.indexOf('not confirm') >= 0) {
-    toast.error('Usuário não bloqueado', toastConfig);
+    toast.error('Cliente inativo', toastConfig);
   } else if (message.indexOf('Invalid verification') >= 0) {
     toast.error('Código inválido');
   } else if (message.indexOf('Username should be an email') >= 0) {
-    toast.success('O usuário precisa ser um email!', toastConfig);
+    toast.success('O cliente precisa ser um email!', toastConfig);
   } else if (message.indexOf('success') >= 0) {
     toast.success('Sucesso!', toastConfig);
   }
